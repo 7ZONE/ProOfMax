06096d2dd303f79dbd049a5efa3941f35c2c7cdc
ZHENGZHIQIANG <zhengzhiqiang@cnlab.com>
2017-09-12 17:11:55 +0800

    first obj

---
+++ ProOfMax/ProOfMax.cpp
@@ -0,0 +1,14 @@
+// ProOfMax.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "this is the first program i creat" << endl;
+    return 0;
+}
+
